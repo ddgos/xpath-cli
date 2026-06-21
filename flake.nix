b53f4cc03cbdeda5200639bd69fdf0ca390563bb
Daniel Guest <daniel.david.guest@gmail.com>
2026-06-21 23:30:13 +0100

    Created nix flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Evaluate xpath expressions on XML or HTML documents";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            rustToolchain
+            pkgs.libxml2
+            pkgs.pkg-config
+          ];
+
+          RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
+          RUST_BACKTRACE = 1;
+        };
+      }
+    );
+}
