9b6cfcfc21041481d7701ac5f3a87719c074f8a3
Daniel Guest <daniel.david.guest@gmail.com>
2026-06-22 07:34:06 +0100

    Reused nixpkgs for rust overlay

--- flake.nix
+++ flake.nix
@@ -3,7 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     flake-utils.url = "github:numtide/flake-utils";
   };
 
