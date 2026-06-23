116a06257f514f54b3ef6daeaf8e2bd909ade5bb
Daniel Guest <daniel.david.guest@gmail.com>
2026-06-23 21:17:51 +0100

    moved to naersk for building package

--- flake.nix
+++ flake.nix
@@ -3,30 +3,38 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
+    naersk = {
+      url = "github:nix-community/naersk";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
+  outputs = { nixpkgs, naersk, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs { inherit system overlays; };
-        rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            rustToolchain
-            pkgs.libxml2
-            pkgs.pkg-config
-          ];
+        pkgs = (import nixpkgs) {
+          inherit system;
+        };
+        naersk' = pkgs.callPackage naersk { };
+        nativeBuildInputs = with pkgs; [
+            libxml2
+            pkg-config
+        ];
+    in
+      rec {
+        packages.default = naersk'.buildPackage {
+          src = ./.;
+          inherit nativeBuildInputs; 
+        };
 
-          RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
-          RUST_BACKTRACE = 1;
+        devShell = pkgs.mkShell {
+          inputsFrom = [ packages.default ];
+          packages = with pkgs; [
+            rustc
+            cargo
+            clippy
+          ];
         };
       }
     );
